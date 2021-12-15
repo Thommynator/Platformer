e92c5f43f0ae9b546a259d9a2503f3ef81735743
Thommynator <Thommynator@users.noreply.github.com>
2021-12-15 19:06:30 +0100

    Adds weapon collection FX

--- Assets/Scripts/EquipWeapon.cs
+++ Assets/Scripts/EquipWeapon.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class EquipWeapon : MonoBehaviour
 {
+    public GameObject particleEffectPrefab;
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag == "Player")
         {
             collider.GetComponent<PlayerMovement>().EquipWeapon(GetComponent<WeaponSpecs>());
+            GameObject particleEffect = Instantiate(particleEffectPrefab, transform.parent);
+            particleEffect.transform.SetParent(null);
             Destroy(transform.parent.gameObject);
         }
     }
+
 }
